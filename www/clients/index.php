aae8ab3de5e5818f99cddb6565b5630cac201ddc
TimmyC2024 <timmyc2024@timcarr.co.uk>
2024-10-30 23:21:58 +0000

    023 This maybe a bogus optionnnnn

---
+++ www/clients/index.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../assets/styles/index.css">
+    <script src="../assets/javascript/script.js" async defer></script>
+    <script> window.name = "clients" </script>
+    <?php include_once '../assets/php/host.php' ?>
+    <!-- <base href="pindex/pindex.club" -->
+    <!-- https://favicon.io/favicon-generator/ -->
+        <link rel="apple-touch-icon" sizes="180x180" href="./assets/favicons/apple-touch-icon.png">
+        <link rel="icon" type="image/png" sizes="32x32" href="./assets/favicons/favicon-32x32.png">
+        <link rel="icon" type="image/png" sizes="16x16" href="./assets/favicons/favicon-16x16.png">
+        <link rel="manifest" href="./assets/favicons/site.webmanifest">
+    
+    <title>clients</title>
+
+</head>
+<!-- <body> -->
+    
+<body onload="hello()">
+    <?php include_once '../assets/php/header.php';?>
+
+    <h1>Testing out client details and registration</h1>
+</body>
+</html>
